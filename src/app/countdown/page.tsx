82f9e18708a027100791b90649439eb3059a0c2a
MudasserHaryani <Mudasser.haryani71@gmail.com>
2024-09-30 02:56:04 +0500

    5th commit

--- src/app/countdown/page.tsx
+++ src/app/countdown/page.tsx
@@ -60,11 +60,12 @@ function CountdownTimer() {
     };
   }, [isActive, timeLeft]);
 
-  const formatTime = (time) => {
+  const formatTime = (time: number) => {
     const minutes = Math.floor(time / 60);
     const seconds = time % 60;
     return `${minutes}:${seconds < 10 ? `0${seconds}` : seconds}`;
-  };
+};
+
 
   return (
     <div>
